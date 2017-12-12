62c6fd670ff6a902ec50f8c6cf6b9d7674b84912
HWEJIN <hwejin23@gmail.com>
2017-12-12 16:44:39 +0900

    -

--- Tools/DataLoader.py
+++ Tools/DataLoader.py
@@ -1,48 +1,9 @@
 import torch
 import torch.utils.data as data
 import torchvision.transforms as transforms
-import Augmentation as AUG
-from scipy.misc import imread, imresize
+import Tools_Augmentation as AUG_T
 import Tools_Image as IMG_T
 import numpy as np
-
-class my_dataset_byPath(data.Dataset):
-    def initialize(self, nodule_list):
-        self.nodule_list = nodule_list
-        self.size = len(nodule_list)
-
-    def __getitem__(self, idx):
-        img_tensor_list = []
-        path = self.nodule_list[idx]['path']
-        label = self.nodule_list[idx]['type']
-            
-        img_3D = np.load(path)
-        if img_3D.shape[0] != 64 or img_3D.shape[1] != 64 or img_3D.shape[2] != 64:
-            img_3D = AUG.makeSquare(img_3D, 64)
-        img_3D = AUG.randomCrop(img_3D)
-        
-        if label == '1':
-            img_3D = AUG.rotate_3D(img_3D)
-        img3D_100, img3D_75, img3D_50 = converter3D(img_3D)
-        img2D = converter2D(img_3D)
-        
-        img_tensor_list.append(img3D_100)
-        img_tensor_list.append(img3D_75)
-        img_tensor_list.append(img3D_50)
-        img_tensor_list.append(img2D)
-        
-        
-        label_tensor = torch.LongTensor([int(label)])
-        
-        return img_tensor_list, label_tensor
-    
-    def __len__(self):
-        return self.size
-    
-    
-    
-
-    
 
 class my_dataset_byInfo(data.Dataset):
     def initialize(self, CandidateList, PatientList):
@@ -57,7 +18,7 @@ class my_dataset_byInfo(data.Dataset):
         img_3D = AUG.randomCrop(img_3D, size=64)
         if label == '1':
             img_3D = AUG.rotate_3D(img_3D)
-        img3D_50, img3D_75, img3D_100 = converter3D_NEW(img_3D)
+        img3D_50, img3D_75, img3D_100 = converter3D(img_3D)
         
         img2D = converter2D(img_3D)
         img_tensor_list.append(img3D_50)
@@ -73,23 +34,8 @@ class my_dataset_byInfo(data.Dataset):
     def __len__(self):
         return self.size
 
-    
-def converter3D(img_3D):
-    img3D_100 = torch.from_numpy(img_3D).float()
-    img3D_100 = img3D_100.view(1, 64, 64, 64)
-            
-    zoom_img = AUG.zoomIn_3D(img_3D, 0.75)
-    img3D_75 = torch.from_numpy(zoom_img).float()
-    img3D_75 = img3D_75.view(1, 64, 64, 64)
-        
-        
-    zoom_img = AUG.zoomIn_3D(img_3D, 0.5)
-    img3D_50 = torch.from_numpy(zoom_img).float()
-    img3D_50 = img3D_50.view(1, 64, 64, 64)
-        
-    return img3D_50, img3D_75, img3D_100
 
-def converter3D_NEW(img_3D):
+def converter3D(img_3D):
     img_64 = img_3D
     img_48 = AUG.getSmallImage_3D(img_3D, 48)
     img_32 = AUG.getSmallImage_3D(img_3D, 32)
@@ -118,7 +64,6 @@ def converter2D(img_3D, size2D=64):
     img2D_Y = np.expand_dims(img2D_Y, axis=0)
     img2D_Z = np.expand_dims(img2D_Z, axis=0)
     img2D = np.concatenate((img2D_X, img2D_Y, img2D_Z), axis = 0) 
-    #img2D = imresize(img2D, (size2D, size2D))
     img2D = torch.from_numpy(img2D).float()
         
     return img2D
--- Tools/Tools_Augmentation.py
+++ Tools/Tools_Augmentation.py
@@ -1,6 +1,4 @@
 import numpy as np
-from scipy.ndimage import rotate
-from scipy .ndimage import zoom
 import random
 
 
@@ -11,23 +9,12 @@ def makeSquare(img_3D, size):
 
     return pad_img
 
-
-def zoomIn_3D(img_3D, ratio):
-    length = img_3D.shape[0]
-    start, end = getSliceIdx(length, ratio)
-    small_img = img_3D[start:end, start:end, start:end]
-    resize_ratio = float(length) / float(small_img.shape[0])
-    resize_img = zoom(small_img, resize_ratio)
-
-    return resize_img
-
 def getSmallImage_3D(img3D, size):
     x, y, z = findMidPoint(img3D)
     smallSize = size / 2
     smallImage = img3D[x-smallSize:x+smallSize, x-smallSize:x+smallSize, x-smallSize:x+smallSize]
     
     return smallImage
-
 
 def rotate_3D(img_3D):
     axis_rand = random.randrange(0, 3)
@@ -36,7 +23,6 @@ def rotate_3D(img_3D):
     img_rotate = np.rot90(img_3D, rot_rand, axis_list[axis_rand]).copy()
 
     return img_rotate
-
 
 def cutAxis(img_3D):
     x, y, z = findMidPoint(img_3D)
@@ -54,13 +40,11 @@ def findMidPoint(img_3D):
 
     return x, y, z
 
-
 def getSliceIdx(length, ratio):
     start = int((length - (length * ratio)) / 2)
     end = length - start
 
     return start, end
-
 
 def randomCrop(img_3D, size):
     rand = random.randrange(0, (img_3D.shape[0] - size))
