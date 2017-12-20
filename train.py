39eb184c97de831338ecbeee007a9c973432c774
HWEJIN <hwejin23@gmail.com>
2017-12-20 14:20:49 +0900

    -

--- train.py
+++ train.py
@@ -23,7 +23,7 @@ def train(model_idx, test_index, batch_size, img_size, isContinue=False):
 
     
     model, model_name, batch_size = MODEL_T.model_setter(model_idx)
-    model_path, model_epoch, previous_batch_size, previous_learning_rate = MODEL_T.modelLoader(model_name, test_index)
+    model_path, model_epoch, previous_batch_size, previous_learning_rate = MODEL_T.modelLoader(model_name, test_index, img_size)
 
  
 
