ec09a8f6905b4788258817fad30daf3dc1c8ff9c
HWEJIN <hwejin23@gmail.com>
2018-01-12 23:00:39 +0900

    -

--- CFG/densenet3D.py
+++ CFG/densenet3D.py
@@ -182,10 +182,7 @@ def generate_3DDensenet(model_name, model_depth, img_size, num_class, isPretrain
     
     
     
-    
-    print isPretrained, '!@!@!@!@'
-    print type(isPretrained)
-    if isPretrained:
+    if isPretrained == True:
         files = os.listdir('../Model/PretrainedWeight')
         model_path = None
         for file in files:
